3e39f09f7756ec64b36495a5222bbb8c92d0c449
Farouk Hamadi <faroukhamadi1@outlook.com>
2022-02-23 05:00:55 +0100

    first commit

---
+++ src/components/Main.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+    </div>
+  );
+};
+
+export default Main;
